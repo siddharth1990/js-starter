bed41d69a661c6c89bf7cbe8cab270ba9c35da38
Siddharth <mishrasiddharth00@gmail.com>
2025-08-28 05:31:47 +0000

    Data type summary

---
+++ datatypes-summary.js
@@ -0,0 +1,44 @@
+// Based on how value is stored and accessed 
+// data types are of two types
+
+
+// Primitive
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraj", "doga"]
+
+let myobj = {
+    name: "Siddharth",
+    age: 35
+}
+
+const myFunction = function () {
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof myFunction);
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
